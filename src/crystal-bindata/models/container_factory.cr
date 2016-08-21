ef105d44cb9ec89e14c5a3610565f06635cf5224
hkdnet <hkdnet@users.noreply.github.com>
2016-08-21 14:03:05 +0900

    Add exclude files
    - .git
    - .gitmodules
    - .gitignore

--- src/crystal-bindata/models/container_factory.cr
+++ src/crystal-bindata/models/container_factory.cr
@@ -1,10 +1,11 @@
 module Crystal::Bindata::Models
   module ContainerFactory
+    EXCLUDE_PATTERNS = %w(.git .gitmodules .gitignore)
     def self.create(src_dir, namespace = "", class_name = "data")
       c = Container.new(namespace, class_name)
       Dir.cd(src_dir) do
         Dir.foreach(".") do |filename|
-          if File.file?(filename)
+          if !EXCLUDE_PATTERNS.includes?(filename) && File.file?(filename)
             resource = Resource.new(filename, File.read(filename))
             c.add_resource(resource)
           end
